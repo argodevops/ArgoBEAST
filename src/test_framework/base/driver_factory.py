8b9ca78de281946639c5d942fb44f21a731da5ef
paul.s <paul.s@argodevops.co.uk>
2025-11-26 16:30:55 +0000

    chore: added support for remote drivers (required for gitpod)

--- src/test_framework/base/driver_factory.py
+++ src/test_framework/base/driver_factory.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-
-'''
-Base class for driver factory
-'''
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class WebDriverFactory():
     def __init__(self, config):
-        self.driver = None
+        self.driver: WebDriver | None = None
         self.options = None
         self.config = config
 
@@ -16,27 +13,36 @@ class WebDriverFactory():
         Create and return a WebDriver instance based on the configuration
         :return: WebDriver instance
         """
-        self.options = webdriver.ChromeOptions()
-        if self.config["headless"]:
-            self.options.add_argument("--headless-new")
 
-        # width and height.
+        self.options = webdriver.ChromeOptions()
+
+        if self.config["headless"]:
+            self.options.add_argument("--headless=new")
+
+        # width and height
         width, height = self.config["window_size"].split(",")
         self.options.add_argument(f"--window-size={width},{height}")
 
-        # Initiate driver with waits and timeouts
-        self.driver = webdriver.Chrome(options=self.options)
+        # Shared args for Docker / Gitpod
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--disable-dev-shm-usage")
+
+        if "remote_url" in self.config and self.config["remote_url"]:
+            # Running inside Docker / Gitpod
+            self.driver = webdriver.Remote(
+                command_executor=self.config["remote_url"],
+                options=self.options
+            )
+        else:
+            # Local laptop mode
+            self.driver = webdriver.Chrome(options=self.options)
+
         self.driver.implicitly_wait(self.config["implicit_wait"])
         self.driver.set_page_load_timeout(self.config['page_load_timeout'])
-
-        # Call base url
         self.driver.get(self.config["base_url"])
 
         return self.driver
 
     def quit_driver(self):
-        """
-        Quit the WebDriver instance
-        """
         if self.driver:
             self.driver.quit()
